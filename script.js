159cb66842ea60c466967f34c9ec5fe29b8ec022
NettoJM <nettojm86@gmail.com>
2022-09-14 23:32:12 -0300

    Adding some code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const positionEmail = document.getElementsByClassName('email')[0];
 const positionSenha = document.getElementsByClassName('senha')[0];
 const positionButton = document.getElementsByClassName('button')[0];
+const textAreaEl = document.getElementById('textarea');
+const counterEl = document.getElementById('counter');
 
 function logPassword() {
   if (positionEmail.value === 'tryber@teste.com' && positionSenha.value === '123456') {
@@ -10,3 +12,9 @@ function logPassword() {
   }
 }
 positionButton.addEventListener('click', logPassword);
+
+function updateCounter() {
+  counterEl.textContent = Math.abs(textAreaEl.value.length - 500);
+}
+
+textAreaEl.addEventListener('input', updateCounter);
